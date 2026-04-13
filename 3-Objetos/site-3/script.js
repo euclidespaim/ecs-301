828025846229c86ef698836b78b391980d70497d
Euclides Paim <euclidespaim@gmail.com>
2026-04-13 07:39:00 -0300

    desenvolvimento web

--- 3-Objetos/site-3/script.js
+++ 3-Objetos/site-3/script.js
@@ -1,8 +1,28 @@
 function atualizarInfoGeral() {
     document.getElementById('nome').innerText = sistemaLab.nome;
     document.getElementById('resp').innerText = `Responsável: ${sistemaLab.responsavel}`;
-    
-    document.getElementById('total').innerText = "Total de equipamentos no Lab: " + sistemaLab.contarEquipamentos();
+    document.getElementById('escola').innerText = `Escola: ${sistemaLab.escola}`
+    document.getElementById('total').innerText = `Total de equipamentos no lab: ${sistemaLab.contarEquipamentos()}`
 }
 
+function redenrizarLista(){
+    const cartao = document.getElementById('cartao');
+    cartao.innerHTML = "";
+
+    sistemaLab.equipamentos.forEach( item => {
+
+        const saude = sistemaLab.verificarSaude(item.horasUso)
+
+        cartao.innerHTML += `
+            <div class="container">
+                <h3>${item.tipo}</h3>
+                <p>Marca: ${item.marca}</p>
+                <p>Uso: ${item.horasUso}</p>
+                <span class="status">${saude}</span>
+            </div>
+        `
+    })
+}
+
+redenrizarLista()
 atualizarInfoGeral()
