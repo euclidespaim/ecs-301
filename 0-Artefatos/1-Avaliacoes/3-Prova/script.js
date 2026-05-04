f09aa413f79e86add197f5573af975eb5467de83
Euclides Paim <euclidespaim@gmail.com>
2026-05-04 09:45:19 -0300

    Projetos de Vibe Code!

--- 0-Artefatos/1-Avaliacoes/3-Prova/script.js
+++ 0-Artefatos/1-Avaliacoes/3-Prova/script.js
@@ -2,12 +2,27 @@ class SistemaAC  {
     constructor() {
         Object.seal(this); // Impede adição ou remoção de propriedades, mas permite alteração dos valores existentes
     }
-    
+
     #temp = 22; // O # define um propriedade interna ES2022 (private field)
 
     // MISSÃO 1: Criar o SETTER 'temperatura(valor)'
     // Use if/else para validar o limite de 16 a 30.
 
+    // MISSÃO 1: O SETTER (O Filtro de Segurança)
+    set temperatura(valor) {
+        // Validação: Somente entre 16 e 30 graus
+        if (valor >= 16 && valor <= 30) {
+            this.#temp = valor;
+        } else {
+            alert("⚠️ Erro: Temperatura fora dos limites de segurança (16°C a 30°C)!");
+        }
+    }
+
+    // MISSÃO 2: O GETTER (O Formatador do Visor)
+    get temperatura() {
+        // Devolve o dado real com a unidade de medida
+        return this.#temp + "°C";
+    }
     
     // MISSÃO 2: Criar o GETTER 'temperatura()'
     // Deve retornar o valor seguido da unidade "°C".
